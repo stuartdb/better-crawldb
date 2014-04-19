4573261e9a66b29ce6fc906879de01f92fd4eae4
Stuart Baker <sdb@stuartdb.com>
2014-04-19 12:26:53 +1200

    Added first version which replaces entry titles
    This first version prepends a special character to the entry titles
    so it becomes easier to use the built in browser search to just search
    for entries and not the entry content.
    
    It breaks internal links though.

---
+++ better-crawldb.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name Better CrawlDB
+// @version 0.1
+// @namespace https://github.com/stuartdb/better-crawldb
+// @author Stuart Baker
+// @description Changes the crawl learndb titles to make them searchable.
+// @include *://loom.shalott.org/*
+// @downloadURL https://github.com/stuartdb/better-crawldb/raw/master/better-crawldb.user.js
+// @updateURL https://github.com/stuartdb/better-crawldb/raw/master/better-crawldb.meta.js
+// @run-at document-end
+// ==/UserScript==
---
+++ better-crawldb.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name Better CrawlDB
+// @version 0.1
+// @namespace https://github.com/stuartdb/better-crawldb
+// @author Stuart Baker
+// @description Changes the crawl learndb titles to make them searchable.
+// @include *://loom.shalott.org/*
+// @downloadURL https://github.com/stuartdb/better-crawldb/raw/master/better-crawldb.user.js
+// @updateURL https://github.com/stuartdb/better-crawldb/raw/master/better-crawldb.meta.js
+// @run-at document-end
+// ==/UserScript==
+(function () {
+    'use strict';
+    var i = 0,
+        pre = '~',
+        titles = document.getElementsByTagName('dt');
+
+    for (i = 0; i < titles.length; i += 1) {
+        titles[i].textContent = pre + titles[i].textContent;
+    }
+
+}());
